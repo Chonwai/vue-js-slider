478e64e7d652dd718d38fc46126c14e4f85018f0
Edison Un <chonwaiun@gmail.com>
2019-08-08 16:38:57 +0800

    Add some customize function: images, image width, image height, button width, button height and slider title. By Edison Un at 2019/08/08 16:40

--- Project/VueSliderComponent.vue
+++ Project/VueSliderComponent.vue
@@ -1,16 +1,139 @@
 <template>
-    <div>
-        Hello!
+  <div class="slider-component">
+    <h2 class="slider-title">{{title}}</h2>
+    <div class="slider-container overflow-hidden flex flex-row">
+      <div
+        class="slider-imgs overflow-hidden flex flex-row"
+        :style="{width: (imgWidth * windowSize) + 'px'}"
+      >
+        <div class="img-element" v-for="img in slideImgs" :key="img">
+          <img
+            :style="{width: imgWidth + 'px', height: imgHeight + 'px', transform: 'translateX' + '(' + currentOffset + 'px' + ')', transition: '0.3s'}"
+            :src="img"
+          />
+        </div>
+        <button class="left-btn" :style="{width: buttonWidth + 'px', height: buttonHeight + 'px'}" @click="moveSlider(-1)">&#10094;</button>
+        <button class="right-btn" :style="{width: buttonWidth + 'px', height: buttonHeight + 'px'}" @click="moveSlider(1)">&#10095;</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import Plugin from './index';
+import Plugin from "./index";
 export default {
-    name: 'VueSliderComponent'
-}
+  name: "VueSliderComponent",
+  props: {
+    title: {
+      type: String,
+      default: "This is a VueSliderComponent title"
+    },
+    slideImgs: Array,
+    imgWidth: {
+      type: Number,
+      default: 360
+    },
+    imgHeight: {
+      type: Number,
+      default: 480
+    },
+    windowSize: {
+      type: Number,
+      default: 3
+    },
+    buttonWidth: {
+      type: Number,
+      default: 36
+    },
+    buttonHeight: {
+      type: Number,
+      default: 36
+    }
+  },
+  data() {
+    return {
+      count: 0,
+      currentOffset: 0
+    };
+  },
+  methods: {
+    moveSlider(direction) {
+      if (direction < 0 && this.currentOffset < 0) {
+        this.currentOffset = this.currentOffset + this.imgWidth;
+      } else if (
+        direction > 0 &&
+        this.currentOffset > (this.count - this.windowSize) * this.imgWidth * -1
+      ) {
+        this.currentOffset = this.currentOffset - this.imgWidth;
+      }
+    }
+  },
+  created() {
+    this.count = this.slideImgs.length;
+  },
+  mounted() {
+    this.count = this.slideImgs.length;
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+}
 
+.slider-container {
+  justify-content: center;
+}
+
+.slider-imgs {
+  position: relative;
+  align-items: center;
+
+  button {
+    position: absolute;
+    border-radius: 100%;
+    background-color: white;
+    border: 1px solid lightgray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .left-btn {
+    left: 0px;
+  }
+
+  .right-btn {
+    right: 0px;
+  }
+}
+
+.overflow-hidden {
+  overflow: hidden;
+}
+
+.overflow-scroll {
+  overflow: scroll;
+}
+
+.flex {
+  display: flex;
+}
+
+.flex-row {
+  flex-direction: row;
+}
+
+.img-element {
+  transition: 0.3s;
+
+  img {
+    object-fit: cover;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    padding-left: 6px;
+    padding-right: 6px;
+  }
+}
 </style>
--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <VueSlider />
+    <VueSlider :imgWidth="280" :imgHeight="360" :windowSize="4" title="Title 1" :slideImgs="imgs" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      imgs: [
+        "https://media.wmagazine.com/photos/5cd33aa1c61fc00ebc7a9d5a/4:3/w_1536/Screen%2520Shot%25202019-05-08%2520at%25204.18.42%2520PM.png",
+        "https://cdn.vox-cdn.com/thumbor/81dix4pSzNqGq5Lq0PYmALAtWPc=/0x0:1920x803/1200x800/filters:focal(1140x0:1446x306)/cdn.vox-cdn.com/uploads/chorus_image/image/62934392/detective_pikachu_trailer_25_1920.0.jpg",
+        "https://img.youtube.com/vi/1roy4o4tqQM/maxresdefault.jpg",
+        "https://i1.wp.com/metro.co.uk/wp-content/uploads/2019/05/aneesh-arts-main-render-image-01-00000-a48a.jpg?quality=90&strip=all&zoom=1&resize=644%2C429&ssl=1",
+        "https://i.ytimg.com/vi/Svil34VCuoE/maxresdefault.jpg",
+        "https://image-cdn.hypb.st/https%3A%2F%2Fhk.hypebeast.com%2Ffiles%2F2019%2F05%2Fpokemon-go-detective-pikachu-additional-content-news-1.jpg?q=75&w=800&cbr=1&fit=max",
+        "https://s5.cdn.memeburn.com/wp-content/uploads/2019/05/1-34-1024x576.jpg"
+      ]
+    };
+  },
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
